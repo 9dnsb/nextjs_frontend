782704803837d3ec1c70f446288b26a8c9612a4b
David Blatt <davidblatt10@gmail.com>
2022-11-01 14:36:19 -0400

    fix hydration issue

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,23 +4,17 @@ import '@fontsource/lato'
 import '@fontsource/kanit/400.css'
 
 import type { AppProps } from 'next/app'
-import { Client, HydrationProvider, Server } from 'react-hydration-provider'
 
 import NavBar from '@/components/NavBar/NavBar'
 
 function MyApp({ Component, pageProps }: AppProps) {
   // eslint-disable-next-line react/jsx-props-no-spreading
   return (
-    <HydrationProvider>
-      <div className="container">
-        <Server>
-          <NavBar />
-        </Server>
-        <Client>
-          <Component {...pageProps} />
-        </Client>
-      </div>
-    </HydrationProvider>
+    <div className="container">
+      <NavBar />
+
+      <Component {...pageProps} />
+    </div>
   )
 }
 
